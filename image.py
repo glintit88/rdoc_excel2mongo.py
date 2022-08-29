c151d4ff2cee83f2234bfbcbf362d79e03bba1b4
Macroview <43488023+glintit88@users.noreply.github.com>
2022-08-29 14:11:56 +0800

    Add files via upload

--- image.py
+++ image.py
@@ -4,28 +4,19 @@ import time
 import os
 from tkinter import *      
 from PIL import ImageTk,Image
+import webbrowser
 
-ws = Tk()
-ws.title('PythonGuides')
-ws.geometry('500x500')
 
-canvas = Canvas(
-    ws,
-    width = 500, 
-    height = 500
-    )      
-canvas.pack()
-
-#folder_path = os.path.join(os.path.dirname(__file__), "image")
-#folder_path = os.getcwd()
-folder_path = 'C:\\Users\\terencema\\Desktop\\MouseWithoutBorders\\image'
-file_type = r'\*.jpg'
-files = glob.glob(folder_path + file_type)
-
-max_file = max(files, key=os.path.getctime)
-print (folder_path)
-c = 0
-while True:
+def loop1():
+    folder_path = 'C:\\Users\\terencema\\Desktop\\MouseWithoutBorders\\image'
+    file_type = r'\*.jpg'
+    files = glob.glob(folder_path + file_type)
+    max_file = max(files, key=os.path.getctime)
+    print (folder_path)
+    c = 0
+    new =1
+    url = "https://www.pythgonguides.com"
+    
     if [f for f in os.listdir(folder_path) if not f.startswith('.')] != []:
         # Some Work
         canvas = Canvas(
@@ -48,12 +39,65 @@ while True:
                 image=img
                 )
         canvas.pack()
-        ws.mainloop()
-
+        print(img)
         time.sleep(1)   # Its just to wait if 'Some Work' is very small
     else:
         print('Empty')
         time.sleep(1)
 
+def openbrowser():
+    url = "https://www.pythonguides.com"
+    webbrowser.open(url,new=1)
 
+#bg = PhotoImage(file = 'image/test.png')
+
+
+ws = Tk()
+ws.title('RDOC Login')
+ws.geometry('500x500')
+
+canvas = Canvas(
+    ws,
+    width = 500, 
+    height = 500
+    )      
+canvas.pack(fill='both', expand = True)
+
+'''
+canvas.create_image(
+	0, 
+	0, 
+	image=bg,
+	anchor = "nw"
+	)
+'''
+canvas.create_text(
+	250, 
+	150, 
+	text = 'RDOC Info',
+	font=('Arial', 50),
+	)
+
+btn = Button(
+	ws, 
+	text = 'EXPLORE MORE',
+	command=openbrowser,
+	width=20,
+	height=2,
+	relief=SOLID,
+	font=('arial', 18)
+	)
+
+btn_canvas = canvas.create_window(
+	100, 
+	200,
+	anchor = "nw",
+	window = btn,
+	)
+
+
+loop1()
+#folder_path = os.path.join(os.path.dirname(__file__), "image")
+#folder_path = os.getcwd()
 ws.mainloop()
+
